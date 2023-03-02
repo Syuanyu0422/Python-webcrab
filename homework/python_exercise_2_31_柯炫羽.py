3ce59e1824987c312b4eec96035d9ae9ccc4ac12
hyunyu <hcl3720977@gmail.com>
2023-03-02 21:45:53 +0800

    rm

--- homework/python_exercise_2_31_柯炫羽.py
+++
@@ -1,4 +0,0 @@
-phrase = input('請輸入字串:')
-print(phrase.upper())
-print(phrase.title())
-
--- homework/python_exercise_3_31_柯炫羽.py
+++
@@ -1,7 +0,0 @@
-def compute(x,y):
-    value=x*y
-    return value
-x=eval(input('請輸入第一個數:'))
-y=eval(input('請輸入第二個數:'))
-
-print(compute(x,y))
